5499ddb7d52a22871a6068972f5409dd83479574
Heri <hlondono@misena.edu.co>
2021-09-25 16:11:58 -0500

    Inicio proyecto G32

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioPaciente _repoPaciente=new RepositorioPaciente(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //AddPaciente();
+            BuscarPaciente(1);
+        }
+        private static void AddPaciente()
+        {
+            var paciente =new Paciente
+            {
+                Nombre="Adriana",
+                Apellidos="Vargas",
+                NumeroTelefono="3002152014",
+                Genero=Genero.Femenino,
+                Direccion="cra 41 22-20 sur",
+                Ciudad="Bogotá"
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+        private static void BuscarPaciente(int idPaciente)
+        {
+            var paciente = _repoPaciente.GetPaciente(idPaciente);
+            Console.WriteLine("Nombre: "+paciente.Nombre+" "+paciente.Apellidos+"  Género: "+paciente.Genero);
+        } 
+    }
+}
